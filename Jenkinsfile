6a46d22df12d5d460db707fad1f1af5cd046e4d4
Marcos Freccia <marcosfreccia@hotmail.com>
2020-09-04 11:56:27 +0200

    Terraform

---
+++ Jenkinsfile
@@ -0,0 +1,2 @@
+String prodCredentials = 'AWS_CREDENTIALS_PROD'
+String devCredentials = 'AWS_CREDENTIALS_DEV'
---
+++ s3.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket" "mfreccia_s3_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+  tags = var.tags
+}
